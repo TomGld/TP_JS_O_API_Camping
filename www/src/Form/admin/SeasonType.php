82e2a392f528cb0c2cddbc6d05cbe7954a993a53
TomGld <fouet.tom.pro@gmail.com>
2025-02-17 11:56:30 +0100

    Ajout Season CRUD; Changement titres btn retour liste; ajout commentaires.

---
+++ www/src/Form/admin/SeasonType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Season;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SeasonType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('label')
+            ->add('season_start', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('season_end', null, [
+                'widget' => 'single_text',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Season::class,
+        ]);
+    }
+}
